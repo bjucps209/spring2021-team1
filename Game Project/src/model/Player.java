792d160dbb9c8419fdab9eb7e34b9012eacd085e
Aya <acath358@students.bju.edu>
2021-03-31 22:02:22 -0400

    editing switch

--- Game Project/src/model/Player.java
+++ Game Project/src/model/Player.java
@@ -1,20 +1,24 @@
 class Player{
-    public Player(){
+    State player = State.DRIVING;
 
+    public Player(State Player){
+        this.player = Player;
     }
+    
 
-    switch (State){
-        case1: State = State.JUMP;
-            break;
-        case2: State = State.SPEEDUP;
-            break;
-        case3: State = State.LEFT;
-            break;
-        case4: State = State.RIGHT;
-            break;
+//     switch (State){
+//         case1: State = State.JUMP;
+//             break;
+//         case2: State = State.SPEEDUP;
+//             break;
+//         case3: State = State.LEFT;
+//             break;
+//         case4: State = State.RIGHT;
+//             break;
+//         case5: State = State.DRIVING;
+//             break;
         
 
 
-    }
-}
-}
+//     }
+// }
--- Game Project/src/model/State.java
+++ Game Project/src/model/State.java
@@ -1,5 +1,5 @@
 package model;
 
 public enum State {
-    JUMP, LEFT, RIGHT, SPEEDUP
+    JUMP, LEFT, RIGHT, SPEEDUP, DRIVING
 }
