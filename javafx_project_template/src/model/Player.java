98eddbaae6f23f688437875e3f00e0eaa267c6c2
jorti426 <jorti426@students.bju.edu>
2021-04-03 16:35:14 -0400

    moved

--- javafx_project_template/src/model/Player.java
+++ javafx_project_template/src/model/Player.java
@@ -16,20 +16,20 @@ class Player{
 
     }
 
-    {
-    switch (State){
-        case 1: State = State.JUMP;
-            break;
-        case 2: State = State.SPEEDUP;
-            break;
-        case 3: State = State.LEFT;
-            break;
-        case 4: State = State.RIGHT;
-            break;
-        case 5: State = State.DRIVING;
-            break;
-    }
-    }
+    // {
+    // switch (State){
+    //     case 1: State = State.JUMP;
+    //         break;
+    //     case 2: State = State.SPEEDUP;
+    //         break;
+    //     case 3: State = State.LEFT;
+    //         break;
+    //     case 4: State = State.RIGHT;
+    //         break;
+    //     case 5: State = State.DRIVING;
+    //         break;
+    // }
+    //}
 
 
     public void jumpTo(){
