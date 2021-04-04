bdcce0a1378fad6bf6db4039a7cc95119c35294c
jorti426 <jorti426@students.bju.edu>
2021-04-03 22:34:32 -0400

    highscores

--- javafx_project_template/src/model/playerHighscore.java
+++ javafx_project_template/src/model/playerHighscore.java
@@ -1,5 +1,17 @@
 package model;
 
 public class playerHighscore {
+    int score; 
+    String name; 
+    LevelSequence distance;
+    DifficultyLevel difficulty; 
+
+    playerHighscore(DifficultyLevel diff, LevelSequence seq, String name, int score){
+        this.difficulty = diff;
+        this.distance = seq;
+        this.name = name;
+        this.score = score;
+    }
     
+
 }
