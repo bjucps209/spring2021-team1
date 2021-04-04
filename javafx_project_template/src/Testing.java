ee4d7b175b063dcc97875959ad65ff43f1729f8d
jorti426 <jorti426@students.bju.edu>
2021-04-03 23:02:07 -0400

    changes

--- javafx_project_template/src/Testing.java
+++ javafx_project_template/src/Testing.java
@@ -1,13 +1,15 @@
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+import model.DifficultyLevel;
+import model.LevelSequence;
 
 public class Testing{
     @Test
     public void highscoresOverallTest(){
         highscoresOverall test = new highscoresOverall();
-        playerHighscore ply1 = new playerHighscore()
-        test.addPlayer(player);
+        playerHighscore ply1 = new playerHighscore(model.DifficultyLevel.EASY, model.LevelSequence.FIVE,"joe", 700)
+        test.addPlayer(ply1);
     }
 
 }
