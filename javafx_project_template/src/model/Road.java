bb16255072efd3925cc0719c2a5e881314511d3f
aya2281 <70032599+aya2281@users.noreply.github.com>
2021-04-03 21:09:24 -0400

    Update Road.java

--- javafx_project_template/src/model/Road.java
+++ javafx_project_template/src/model/Road.java
@@ -7,46 +7,74 @@ public class Road{
     public Road(){
         {
             //Switch for each DifficultyLevel/ LevelSequece combinations
-            // switch (DifficultyLevel){
-            // case 1: DifficultyLevel = DifficultyLevel.EASY;
-            //     switch (LevelSequence){
-            //         case 1: LevelSequence = LevelSequence.FIVE;
-            //             break;
-            //         case 2: LevelSequence = LevelSequence.TEN;
-            //             break;
-            //         case 3: LevelSequence = LevelSequence.FIFTEEN;
-            //             break;
-            //        }
-            // case 2: DifficultyLevel = DifficultyLevel.MEDIUM;
-            //     switch (LevelSequence){
-            //         case 1: LevelSequence = LevelSequence.FIVE;
-            //             break;
-            //         case 2: LevelSequence = LevelSequence.TEN;
-            //             break;
-            //         case 3: LevelSequence = LevelSequence.FIFTEEN;
-            //             break;
-            //     }
-            // case 3: DifficultyLevel= DifficultyLevel.HARD;
-            //     switch (LevelSequence){
-            //         case 1: LevelSequence = LevelSequence.FIVE;
-            //             break;
-            //         case 2: LevelSequence = LevelSequence.TEN;
-            //             break;
-            //         case 3: LevelSequence = LevelSequence.FIFTEEN;
-            //     }
-            //}
-        }
+            switch (DifficultyLevel){
+            case 1: DifficultyLevel = DifficultyLevel.EASY;
+                switch (LevelSequence){
+                    case 1: LevelSequence = LevelSequence.FIVE;
+                        break;
+                    case 2: LevelSequence = LevelSequence.TEN;
+                        break;
+                    case 3: LevelSequence = LevelSequence.FIFTEEN;
+                        break;
+                   }
+            case 2: DifficultyLevel = DifficultyLevel.MEDIUM;
+                switch (LevelSequence){
+                    case 1: LevelSequence = LevelSequence.FIVE;
+                        break;
+                    case 2: LevelSequence = LevelSequence.TEN;
+                        break;
+                    case 3: LevelSequence = LevelSequence.FIFTEEN;
+                        break;
+                }
+            case 3: DifficultyLevel= DifficultyLevel.HARD;
+                switch (LevelSequence){
+                    case 1: LevelSequence = LevelSequence.FIVE;
+                        break;
+                    case 2: LevelSequence = LevelSequence.TEN;
+                        break;
+                    case 3: LevelSequence = LevelSequence.FIFTEEN;
+                        break;
+                }
+            }
+            }
+    }
+    
+    /**
+     * @return none
+     * @param TrafficItem:object
+     */
+    public void addObjectsA(){
+        //code
     }
 
-    public void addObjectsA(int x, int y){
-        //setCell(x: int, type: Traffic)
+    /**
+     * @return none
+     * @param TrafficItem:object
+     */
+    public void addObjectsB(){
+        //code
     }
 
-    public void addObjectsB(int x, int y){
-        //setCell(x: int, type: Traffic)
+    /**
+     * @return none
+     * @param TrafficItem:object
+     */
+    public void addObjectsC(){
+        //code
     }
 
-    public void addObjectsC(int x, int y){
-        //setCell(x: int, type: Traffic)
+    /**
+     * @param int:d
+     * @param int:r
+     * @return
+     */
+    public int calculateTime(int d, int r){
+        t = d/t;
+        return t;
+    }
+
+    public Road createRoad(){
+        Road newRoad = new Road();
+        return newRoad;
     }
 }
