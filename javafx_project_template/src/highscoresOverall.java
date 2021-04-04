6d019e303f3892a3e0ebf47bf77bec0a9331ab59
jorti426 <jorti426@students.bju.edu>
2021-04-03 22:57:12 -0400

    highscores

--- javafx_project_template/src/highscoresOverall.java
+++ javafx_project_template/src/highscoresOverall.java
@@ -1,4 +1,3 @@
-package model;
 
 import java.io.FileWriter;
 import java.io.IOException;
--- javafx_project_template/src/playerHighscore.java
+++ javafx_project_template/src/playerHighscore.java
@@ -1,10 +1,9 @@
-package model;
 
 public class playerHighscore {
     int score; 
     String name; 
     LevelSequence distance;
-    DifficultyLevel difficulty; 
+    model.DifficultyLevel difficulty; 
 
     playerHighscore(DifficultyLevel diff, LevelSequence seq, String name, int score){
         this.difficulty = diff;
