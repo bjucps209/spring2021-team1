9bb7cac3a336395ab944b49b998aed211e1a3479
jorti426 <jorti426@students.bju.edu>
2021-04-03 22:40:25 -0400

    highscore

--- javafx_project_template/src/model/highscoresOverall.java
+++ javafx_project_template/src/model/highscoresOverall.java
@@ -1,5 +1,13 @@
 package model;
 
+import java.util.ArrayList;
+
 public class highscoresOverall {
+    
+    ArrayList<Object> players = new ArrayList<Object>();
+
+    public void addPlayer(playerHighscore player){
+
+    }
     
 }
