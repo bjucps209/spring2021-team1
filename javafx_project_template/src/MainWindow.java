09f3c0de85e432435a5234eddce457f960b480af
jorti426 <jorti426@students.bju.edu>
2021-04-08 01:08:33 -0400

    highscores

--- javafx_project_template/src/MainWindow.java
+++ javafx_project_template/src/MainWindow.java
@@ -7,6 +7,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
 import javafx.stage.Stage;
 
 
