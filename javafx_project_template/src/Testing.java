f7c8b8aca88fe4c4f8b3cfac2d05c4feae5d1539
jorti426 <jorti426@students.bju.edu>
2021-04-08 00:56:10 -0400

    HS testing

--- javafx_project_template/src/Testing.java
+++ javafx_project_template/src/Testing.java
@@ -1,48 +1,42 @@
 
-// import java.io.IOException;
+import java.io.IOException;
 
-// import org.junit.Test;
+import org.junit.Test;
 
 
-// public class Testing{
-//     @Test
-//     public void highscoresOverallTest(){
-//         highscoresOverall test = new highscoresOverall();
-//         playerHighscore ply1 = new playerHighscore(model.DifficultyLevel.EASY, model.LevelSequence.FIVE,"William's", 800);
-//         test.addPlayer(ply1);
-//         // test.addToTxtFile();
-//         playerHighscore ply2 = new playerHighscore(model.DifficultyLevel.EASY, model.LevelSequence.FIFTEEN,"Aya", 700);
-//         test.addPlayer(ply2);
-//         // test.addToTxtFile();
+public class Testing{
+    @Test
+    public void highscoresOverallTest() throws IOException{
+        HighscoresOverall test = new HighscoresOverall();
+
         
-//     public void writeToTxtFileText() throws IOException{
-//         PlayerHighScore ply1 = new PlayerHighScore("Jackie", 900);
-//         PlayerHighScore ply2 = new PlayerHighScore("Aya", 1000);
-//         PlayerHighScore ply3 = new PlayerHighScore("Brigs", 700);
+        PlayerHighScore ply1 = new PlayerHighScore("Jackie", 900);
+        PlayerHighScore ply2 = new PlayerHighScore("Aya", 1000);
+        PlayerHighScore ply3 = new PlayerHighScore("Brigs", 700);
 
-//         AllHighScore all = AllHighScore.getInstance();
-//         all.addPlayer(ply1);
-//         all.addPlayer(ply2);
-//         all.addPlayer(ply3);
-//         HighScoreWriter writer = HighScoreWriter.getInstance();
-//         writer.addToTxtFile();
+        AllHighScore all = AllHighScore.getInstance();
+        all.addPlayer(ply1);
+        all.addPlayer(ply2);
+        all.addPlayer(ply3);
+        HighScoreWriter writer = HighScoreWriter.getInstance();
+        writer.addToTxtFile();
 
-//     }
+    }
 
-//     @Test
-//     public void readFromFileTest(){
-//         // HighScoreWriter test = new HighScoreWriter();
-//         // test.readFromTxtFile("highscore.txt");
+    @Test
+    public void readFromFileTest(){
+        // HighScoreWriter test = new HighScoreWriter();
+        // test.readFromTxtFile("highscore.txt");
         
-//     }
+    }
 
-//     @Test
-//     public void testLoad() {
+    @Test
+    public void testLoad() {
         
-//     }
-//     @Test
-//     public void testSave() {
+    }
+    @Test
+    public void testSave() {
         
-//     }
+    }
 
-// }
+}
