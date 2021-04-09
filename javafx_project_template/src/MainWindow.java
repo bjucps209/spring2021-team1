5720e6f45e8922136baf16bcde03f038cb0a37cd
William's Fleurant <wfleu445>
2021-04-09 15:17:04 -0400

    changed LevelDesign and MainWindow.fxml

--- javafx_project_template/src/MainWindow.java
+++ javafx_project_template/src/MainWindow.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.security.Key;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -7,10 +8,11 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import javafx.scene.control.*;
-
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 
 
 
@@ -18,15 +20,20 @@ public class MainWindow {
     
     @FXML Button btnStart, btnHelp, btnAbout, btnHigh;
     @FXML Button btnBackStart, btnBackHelp, btnBackAbout, btnBackHigh;
+    @FXML Pane roadPane;
     
    
 
     @FXML
     void onStartClicked(ActionEvent event) throws IOException {
+
+        
+
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         Parent root = FXMLLoader.load(getClass().getResource("LevelChoice.fxml"));
         Scene scene = new Scene(root);
-        
+
+
         stage.setScene(scene);
         stage.show();
     }
--- javafx_project_template/src/model/LevelDesign.java
+++ javafx_project_template/src/model/LevelDesign.java
@@ -1,28 +1,51 @@
 package model;
-<<<<<<< HEAD
-=======
 
->>>>>>> fa1771db0029da95d27a7347c5e5b1e8b8af29e2
 import java.util.ArrayList;
 
 
 class LevelDesign{
 
+    /**
+     *
+     * @param distance
+     * @return int
+     */
+    public void getsequenceDifficulty(LevelSequence distance){
+        if (distance == LevelSequence.FIVE){
 
-    public int sequenceDifficulty(LevelSequence difficulty){
-//increase timer and in turn increase the speed at which the obstacle appear
-        return 0;
+        }
+        if (distance == LevelSequence.TEN){
+            
+        }
+        if (distance == LevelSequence.FIFTEEN){
+            
+        }
     }
 
-    public int levelDifficulty(DifficultyLevel difficulty){
-//increase timer and in turn increase the speed at which the obstacle appear
-        return 0;
+    public int getlevelDifficulty(DifficultyLevel difficulty){
+            if(difficulty == DifficultyLevel.EASY){
+
+            }
+            if(difficulty == DifficultyLevel.MEDIUM){
+                
+            }
+            if(difficulty == DifficultyLevel.HARD){
+                
+            }
+        
     }
 
 
-public ArrayList<RoadBlock> buildLevel(RoadBlock roadblock){
+
+    public LevelSequence currentSequence(LevelSequence sequence){
+
+
+
+    }
+
+/* public ArrayList<RoadBlock> buildLevel(RoadBlock roadblock){
     //return an arraylist with the user specified oblstacles
     return null;
-}
+} */
 
 }
