1164f8e380b9cb8432132cb69f955b6d468d8309
jorti426 <jorti426@students.bju.edu>
2021-04-10 15:42:18 -0400

    game window

--- javafx_project_template/src/GameWindow.java
+++ javafx_project_template/src/GameWindow.java
@@ -38,13 +38,12 @@ public class GameWindow implements ObserverGame{
 
     // final Image humanImage = new Image("/images/human.gif");
     // final Image potholeImage = new Image("/images/blackhole.gif");
-    final Image humanImage = new Image("/images/shield.gif");
-    final Image potholeImage = new Image("/images/speed.gif");
+    final Image humanImage = new Image("/images/shield.png");
+    final Image potholeImage = new Image("/images/speed.png");
     final Image truckImage = new Image("/images/truck.png");
     final Image coneImage = new Image("/images/cone.png");
     final Image carImage = new Image("/images/RoadBlockcar.png");
 
-<<<<<<< HEAD
     
     public void initialize(){
         Image image = new Image("\\images\\pngfind.com-skateboard-side-view-png-6935593.png");
@@ -52,90 +51,13 @@ public class GameWindow implements ObserverGame{
         imageview.setX(50);
         imageview.setY(50);
         pane.getChildren().add(imageview);
-=======
-
-
-    @FXML
-    public void initialize(){
-
-        Road road = new Road();
-        ArrayList<Obstacle> usingRB = road.getUsingRB();
-
-        var img = new ImageView(IMG_CAR);
-        img.setPreserveRatio(true);
-        img.setFitWidth(100);
-        img.relocate(50 , 300);
-        pane.getChildren().add(img);
-
-        ImageView obstacleImageView;
-
-
-        for(int i = 0; i < usingRB.size(); i++){
-            RoadBlock type = road.getObjectType(usingRB.get(i));
-            
-            if(type == RoadBlock.PEOPLE){
-                obstacleImageView = new ImageView(humanImage);
-                pane.getChildren().add(obstacleImageView);
-            }else if (type == RoadBlock.POTHOLES){
-                obstacleImageView = new ImageView(potholeImage);
-                pane.getChildren().add(obstacleImageView);
-            } else if (type == RoadBlock.TRUCK){
-                obstacleImageView = new ImageView(truckImage);
-                pane.getChildren().add(obstacleImageView);
-            } else if (type == RoadBlock.CONES){
-                obstacleImageView = new ImageView(coneImage);
-                pane.getChildren().add(obstacleImageView);
-            } else if (type == RoadBlock.CARS){
-                obstacleImageView = new ImageView(carImage);
-                pane.getChildren().add(obstacleImageView);
-            }
-
-            
-        }
-
     }
+
+
     @Override
     public void update(ArrayList<Obstacle> list) {
-        //for loop through the arraylist
-        //set the image of the obstacle object to the certain x by doing obstacle.getX()
-        //after creating a method of separating the lanes by abc, set the y of the same image by obstacle.getY();
-        //CHeck for obstacle object type
-    }
-
-    public void keyEvent(KeyEvent event){
-        // Player player = 
-
-        // State[] state = State.values();
-        // for(State s: state){
-        // {
-        //     switch (s){
-        //         case JUMP:
-        //             jumpUp();
-        //         case SPEEDUP:
-        //             break;
-        //             // speedUp();
-        //         case LEFT:
-        //             break;
-        //         case RIGHT:
-        //             break;
-        //         case MOVING:
-        //             break;
-        //     default:
-        //         break;
-        //     }
-        // }
-        // }
-        // // if(event.getCode() == KeyCode.SPACE){
-        //     input.set(State.JUMP);
-        // } else if (event.getCode() == KeyCode.UP){
-        //     input.set(State.LEFT);
-        // } else if (event.getCode() == KeyCode.DOWN){
-        //     input.set(State.RIGHT);
-        // } else if (event.getCode() == KeyCode.RIGHT){
-        //     input.set(State.SPEEDUP);
-        // }
-
->>>>>>> 876f1de791b5711f0370202e11c86f41bfb1a107
+        // TODO Auto-generated method stub
+        
     }
 
     
