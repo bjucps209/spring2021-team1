7ce71f156f74bb5e6158be72236424a5733110a1
jorti426 <jorti426@students.bju.edu>
2021-04-10 15:51:29 -0400

    pane

--- javafx_project_template/src/GameWindow.java
+++ javafx_project_template/src/GameWindow.java
@@ -57,28 +57,3 @@ public class GameWindow implements ObserverGame {
     }
 
 }
-
-// public class GameWindow {
-// @FXML
-// Pane paneMain;
-
-// @FXML
-// Label lblCoord;
-
-// final Image IMG_CAR = new Image("/images/car.png");
-
-// Obstacle obstacle;
-
-// public void initialize() {
-// var img = new ImageView(IMG_CAR);
-// img.setPreserveRatio(true);
-// img.setFitWidth(100);
-// img.relocate(50 , 300);
-// paneMain.getChildren().add(img);
-// img.setOnKeyPressed(e -> keyPress(img););
-// }
-
-// private void keyPress(Node node) {
-
-// }
-// }
